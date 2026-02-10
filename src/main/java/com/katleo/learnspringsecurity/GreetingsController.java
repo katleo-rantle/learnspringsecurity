d1dc891332deec17910b0b930ff14fbfb44b93f0
katleo rantle <kprantle@gmail.com>
2026-02-10 08:53:25 +0200

    create simple controller

---
+++ src/main/java/com/katleo/learnspringsecurity/GreetingsController.java
@@ -0,0 +1,12 @@
+package com.katleo.learnspringsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    @GetMapping(path = "/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
