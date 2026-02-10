20a4d76c3e327ebeddd69e9a56cdd311f9442f4f
katleo rantle <kprantle@gmail.com>
2026-02-10 09:56:30 +0200

    add spring security

--- src/main/java/com/katleo/learnspringsecurity/ch01_basic/GreetingsController.java
+++ src/main/java/com/katleo/learnspringsecurity/ch01_basic/GreetingsController.java
@@ -1,4 +1,4 @@
-package com.katleo.learnspringsecurity;
+package com.katleo.learnspringsecurity.ch01_basic;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
